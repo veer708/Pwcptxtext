21f2ddf6ce7b8ecef798005e7f4e1f37a1dc1767
Gittu2raj11 <152615267+Gittu2raj11@users.noreply.github.com>
2025-05-03 12:09:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "20723624"
 api_hash = "6b2d857266669bab971257d8312af741"
-bot_token = "7969101226:AAH1jPoLSW4SyppdlTK_8Fkt36-jRhwwoEU"
-auth_users = [7777851415]
+bot_token = "8047648005:AAEac3cjeOixVPmJc8mBk03jR7oiyz3GpGA"
+auth_users = [1843979302,6568611832]
